8872fde43dc23ae4c886656d5cba720f1396f127
Apis312 <162021952+Apis312@users.noreply.github.com>
2024-04-07 11:48:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import geometry.Point;
+import geometry.Circle;
+import geometry.ColoredCirlce;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,6 +14,21 @@ public class Main {
 		
 		System.out.println("Współrzędne pliku to:\nx = " + wspX + "\ny = " + wspY);
 
+		
+		Circle kolo = new Circle(punkt1);
+		System.out.println("Promien kola to " + kolo.getRadius() + "\nPole koła to " + kolo.getArea());
+		System.out.println("Obwod kola to " + kolo.calculatePerimeter());
+		double[] tab = kolo.getCenter();
+		System.out.println("Srodek kola znajduje sie w punkcie x = " + tab[0] + " || y = " + tab[1]);
+		
+		ColoredCirlce kolorowe = new ColoredCirlce(punkt1);
+		System.out.println("Kolor koła to " + kolorowe.getColor());
+		
+		//Kod przekopiowany z poprzedniego koła - nie kolorowego
+		System.out.println("Promien kola to " + kolorowe.getRadius() + "\nPole koła to " + kolorowe.getArea());
+		System.out.println("Obwod kola to " + kolorowe.calculatePerimeter());
+		tab = kolorowe.getCenter();
+		System.out.println("Srodek kola znajduje sie w punkcie x = " + tab[0] + " || y = " + tab[1]);
 	}
 
 }
